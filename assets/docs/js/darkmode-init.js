c9591e56e267c5c8dc9bc624979a209be478e0ef
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2024-04-16 15:57:47 +0200

    set the darkmode as default when init

--- assets/docs/js/darkmode-init.js
+++ assets/docs/js/darkmode-init.js
@@ -1,6 +1,8 @@
 const globalDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 const localMode = localStorage.getItem('theme');
 
+localStorage.setItem('theme', 'dark');
+
 if (globalDark && (localMode === null)) {
   localStorage.setItem('theme', 'dark');
   document.documentElement.setAttribute('data-dark-mode', '');
